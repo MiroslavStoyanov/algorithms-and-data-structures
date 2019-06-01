b9f1d1914657833d34b9e95b91b722ef869194c6
Miroslav Stoyanov <miro@torussoft.com>
2019-06-01 11:04:30 +0300

    Initial commit

---
+++ DataStructures/DynamicArrays/DynamicArray.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace DataStructures.DynamicArrays
+{
+    public class DynamicArray<T> where T : class
+    {
+        private T[] array;
+        private int length = 0;
+        private int capacity = 0;
+
+        public DynamicArray(int capacity)
+        {
+            if (this.capacity == 0)
+            {
+                throw new NullReferenceException("Capacity is" + this.capacity);
+            }
+
+            this.capacity = capacity;
+            this.array = (T[]) new object[capacity];
+        }
+
+        public int Size() => this.length;
+
+        public bool IsEmpty() => this.Size() == 0;
+
+        public T Get(int index) => this.array[index];
+
+        public void Set(int index, T element) => this.array[index] = element;
+
+        public void Clear()
+        {
+            for (int i = 0; i < this.length; i++)
+            {
+                this.array[i] = null;
+            }
+
+            this.length = 0;
+        }
+
+        public void Add(T element)
+        {
+            if (this.length + 1 >= this.capacity)
+            {
+                if (this.capacity == 0)
+                {
+                    this.capacity = 1;
+                }
+                else
+                {
+                    this.capacity += 2;
+                }
+
+                var newArray = (T[]) new object[this.capacity];
+
+                for (int i = 0; i < this.length; i++)
+                {
+                    this.array[i] = newArray[i];
+                }
+
+                this.array = newArray;
+            }
+
+            this.array[this.length++] = element;
+        }
+
+        public T RemoveAt(int removeIndex)
+        {
+            if (removeIndex >= this.length || removeIndex < 0)
+            {
+                throw new Exception("Invalid index");
+            }
+
+            T data = this.array[removeIndex];
+            var newArray = (T[]) new object[this.length - 1];
+
+            for (int i = 0, j = 0; i < this.length; i++, j++)
+            {
+                if (i == removeIndex)
+                {
+                    j--;
+                }
+                else
+                {
+                    newArray[j] = this.array[i];
+                }
+            }
+
+            this.array = newArray;
+            this.capacity = --this.length;
+
+            return data;
+        }
+    }
+}
---
+++ DataStructures/LinkedLists/DoublyLinkedList.cs
@@ -0,0 +1,144 @@
+﻿namespace DataStructures.LinkedLists
+{
+    public class DoublyLinkedList<T> where T : class
+    {
+        private int size = 0;
+        private Node<T> head = null;
+        private Node<T> tail = null;
+
+        private class Node<T>
+        {
+            internal T Data;
+            internal Node<T> Prev;
+            internal Node<T> Next;
+
+            public Node(T data, Node<T> prev, Node<T> next)
+            {
+                this.Data = data;
+                this.Prev = prev;
+                this.Next = next;
+            }
+
+            public string ToString() => this.Data.ToString();
+        }
+
+        public void Clear()
+        {
+            Node<T> trav = this.head;
+
+            while (trav != null)
+            {
+                Node<T> next = trav.Next;
+                trav.Prev = trav.Next = null;
+                trav.Data = null;
+                trav = next;
+            }
+
+            this.head = this.tail = null;
+            this.size = 0;
+        }
+
+        public int Size() => this.size;
+
+        public bool IsEmpty() => this.Size() == 0;
+
+        public void AddLast(T element)
+        {
+            if (this.IsEmpty())
+            {
+                this.head = this.tail = new Node<T>(element, null, null);
+            }
+            else
+            {
+                this.tail.Next = new Node<T>(element, this.tail, null);
+                this.tail = this.tail.Next;
+            }
+
+            this.size++;
+        }
+
+        public void AddFirst(T element)
+        {
+            if (this.IsEmpty())
+            {
+                this.head = this.tail = new Node<T>(element, null, null);
+            }
+            else
+            {
+                this.head.Prev = new Node<T>(element, null, this.head);
+                this.head = this.head.Prev;
+            }
+
+            this.size++;
+        }
+
+        public T PeekFirst()
+        {
+            if (this.IsEmpty())
+            {
+                throw new System.Exception("Empty list");
+            }
+
+            return this.head.Data;
+        }
+
+        public T PeekLast()
+        {
+            if (this.IsEmpty())
+            {
+                throw new System.Exception("Empty list");
+            }
+
+            return this.tail.Data;
+        }
+
+        public T RemoveFirst()
+        {
+            if (this.IsEmpty())
+            {
+                throw new System.Exception("Empty list");
+            }
+
+            T data = this.head.Data;
+            this.head = this.head.Next;
+
+            this.size--;
+
+            if (this.IsEmpty())
+            {
+                this.tail = null;
+            }
+            else
+            {
+                this.head.Prev = null;
+            }
+
+            return data;
+        }
+
+        public T RemoveLast()
+        {
+            if (this.IsEmpty())
+            {
+                throw new System.Exception("Empty list");
+            }
+
+            T data = this.tail.Data;
+            this.tail = this.tail.Prev;
+
+            this.size--;
+
+            if (this.IsEmpty())
+            {
+                this.head = null;
+            }
+            else
+            {
+                this.tail.Next = null;
+            }
+
+            return data;
+        }
+    }
+
+}
