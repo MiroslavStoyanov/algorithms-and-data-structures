57ec9ae320dbdd66c1c439a13f635b529dfd6487
Miroslav Stoyanov <miro@torussoft.com>
2019-06-01 11:44:55 +0300

    Added more data structures

---
+++ DataStructures/Queues/Queue.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructures.Queues
+{
+    public class Queue<T> where T : class
+    {
+        private readonly LinkedList<T> list = new LinkedList<T>();
+
+        public Queue()
+        {
+        }
+
+        public Queue(T firstElement)
+        {
+            this.list.AddLast(firstElement);
+        }
+
+        public int Size() => this.list.Count;
+
+        public bool IsEmpty() => this.list.Count == 0;
+
+        public void Poll()
+        {
+            if (this.IsEmpty())
+            {
+                throw new System.Exception("Queue is empty");
+            }
+
+            this.list.RemoveFirst();
+        }
+
+        public void Offer(T element) => this.list.AddLast(element);
+    }
+}
---
+++ DataStructures/Stacks/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructures.Stacks
+{
+    public class Stack<T> where T : class
+    {
+        private readonly LinkedList<T> list = new LinkedList<T>();
+
+        public Stack()
+        {
+        }
+
+        public Stack(T firstElement)
+        {
+            this.Push(firstElement);
+        }
+
+        public bool IsEmpty() => this.list.Count == 0;
+
+        public int Size() => this.list.Count;
+
+        public void Push(T element) => this.list.AddLast(element);
+
+        public void Pop()
+        {
+            if (this.IsEmpty())
+            {
+                throw new Exception("Stack is empty");
+            }
+
+            this.list.RemoveLast();
+        }
+    }
+}
